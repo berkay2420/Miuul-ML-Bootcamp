bfe9627dfabf086100cb4b3b51919e83c1c958a9
Berkay Aydın <99977526+ValyrianAtreides@users.noreply.github.com>
2024-10-05 12:31:52 +0300

    correlation analysis

--- Part-3 (Advanced Functional Eda)/eda.py
+++ Part-3 (Advanced Functional Eda)/eda.py
@@ -1,5 +1,6 @@
 #### Advanced Functional Exploratory Data Analysis (EDA) ####
 
+from re import T
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -324,3 +325,66 @@ for col in num_cols:
 ##################################
 #### Correlation Analysis ########
 ##################################
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = pd.read_csv("datasets/breast_cancer.csv")
+df = df.iloc[:, 1:-1]
+df.head()
+
+num_cols = [col for col in df.columns if df[col].dtype in ["int", "float"] ]
+
+corr = df[num_cols].corr()
+
+sns.set(rc={'figure.figsize':(12,12)})
+sns.heatmap(corr, cmap="RdBu")
+plt.show()
+
+
+#### Deleting High Correlation Values ####
+
+cor_matrix = df.corr().abs()  #abs=absolute=mutlak değer
+
+upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+# 1) Make a np array in shape of cor_matrix with 1's
+# 2) Change types from int to boolean. (1 for True & 0 for False)
+# 3) np.triu return of upper triangle matrix. This new matrix doesn't show correaltions 
+# between same values twice
+########
+# 1) Create a boolean array of the same shape as cor_matrix, where the upper triangle (excluding diagonal) is True
+# 2) Apply this mask to cor_matrix, keeping only the upper triangle correlations (excluding duplicates)
+########
+# Burada k>1 demek orta çaprazdan bir sondaki çaprazı(diagonal) al demek. Çünkü orta çaprazda hepsi kendisiyle
+# eşleşmiş olacakti
+
+drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col]>0.90)]
+# Matris içindeki sütünlardan 90 ve üzeri değerli verilere sahip olanları silmek için. Böyle yapmamızın
+# sebebi eğerl 90 ve üzeri korelasyon varsa zaten bu değişkenler çok benzerdir o yüzden sadece onlardan
+# biriyle işlem yapsak yeterli olacaktır. Ancak bu durum her zaman tercih edilmek zorun da değil
+
+cor_matrix[drop_list] #Variables with high correlation 
+df.drop(drop_list, axis=1) #deleting high correaltion values from dataframe
+
+###
+def high_correlated_cols(dataframe, plot=False, corr_th= 0.90):
+  corr = dataframe.corr()
+  cor_matrix = corr.abs()
+  upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+  drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col]>corr_th)]
+
+  if plot:
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    sns.set(rc={'figure.figsize':(15,15)})
+    sns.heatmap(corr, cmap="RdBu")
+    plt.show()
+
+  return drop_list
+
+drop_list = high_correlated_cols(df, plot=True)
+
+high_correlated_cols(df.drop(drop_list, axis=1), plot=True)
+
