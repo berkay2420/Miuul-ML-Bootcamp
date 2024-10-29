b0b4cb33cd6e9a35d9630cb3527edf5db3289608
Berkay Aydın <99977526+ValyrianAtreides@users.noreply.github.com>
2024-10-29 14:53:12 +0300

    gradient boosting machines

---
+++ Part-5 (Machine Learning)/gradient_boosting_machines_101.py
@@ -0,0 +1,49 @@
+import warnings
+import numpy as np
+import pandas as pd
+import seaborn  as sns
+from matplotlib import pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, export_graphviz, export_text
+from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_validate, validation_curve
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
+from sklearn.model_selection import GridSearchCV, cross_validate, RandomizedSearchCV, validation_curve
+
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+
+pd.set_option('display.max_columns', None)
+warnings.simplefilter(action='ignore', category=Warning)
+
+df = pd.read_csv("diabetes.csv")
+df.head(3)
+
+y = df["Outcome"]
+X = df.drop(["Outcome"], axis=1)
+
+gbm_model = GradientBoostingClassifier(random_state=17)
+
+gbm_model.get_params() 
+#n_estimators burada optimizasyon sayısı ağaç değil
+
+cv_results = cross_validate(gbm_model, X, y, cv=10, scoring=["accuracy","f1","roc_auc"])
+cv_results["test_accuracy"].mean()
+cv_results["test_f1"].mean()
+cv_results["test_roc_auc"].mean()
+
+gbm_params = {"learning_rate": [0.01, 0.1],
+              "max_depth": [3,8,10],
+              "n_estimators":[100,500,1000],
+              "subsample":[1,0.5,0.7]}
+
+gbm_best_grid = GridSearchCV(gbm_model, gbm_params, cv=5, n_jobs=-1, verbose=True).fit(X,y)
+
+print(gbm_best_grid.best_params_)
+
+gbm_final = gbm_model.set_params(**gbm_best_grid.best_params_, random_state=17).fit(X,y)
+
+cv_results = cross_validate(gbm_final, X, y, cv=10, scoring=["accuracy","f1","roc_auc"])
+cv_results["test_accuracy"].mean()
+cv_results["test_f1"].mean()
+cv_results["test_roc_auc"].mean()
